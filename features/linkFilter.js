711580b6e9860ca02ca4a3e80972720c69cf1864
TikoGrantYT <admin@tikogrant.com>
2023-02-04 11:27:05 -0800

    commit

--- features/linkFilter.js
+++ features/linkFilter.js
@@ -1,35 +1,24 @@
+const { EmbedBuilder } = require("discord.js");
 module.exports = client => {
     client.on("messageCreate", (message) => checkMsg(message));
-    client.on("messageEdit", (oldMessage, newMessage) => checkMsg(newMessage));
+    client.on("messageUpdate", (oldMessage, newMessage) => checkMsg(newMessage));
 
-    function checkMsg(message) {
-        if (message.author.bot) return false;
-        if (message.author.id !== "874730179468079159") {
+    function checkMsg(msg) {
+        if (msg.author.bot) return false;
+        if (msg.author.id !== "874730179468079159") {
             const blocked = ["https://", "http://", "discord.gg/"];
 
             for (let i = 0; i < blocked.length; i++) {
                 var checkWord = blocked[i];
 
-                if (message.content.toLowerCase().includes(checkWord)) {
-                    message.delete();
-                    message.channel.send({
-                        content: `<@${message.author.id}> (${message.author.username}#${message.author.discriminator})`,
-                        embeds: [{
-                            color: "ff0000",
-                            description: `<@${message.author.id}> (${message.author.username}#${message.author.discriminator}), links aren't allowed in this server without special permission. Please keep this community fun and safe for everyone!`,
-                        }],
-                        components: [{
-                            type: 1,
-                            components: [
-                                {
-                                    style: 5,
-                                    label: `Read The Rules`,
-                                    url: "https://discord.com/channels/749767042785083451/853327138375598100",
-                                    type: 2,
-                                    disabled: false,
-                                }
-                            ]
-                        }],
+                if (msg.content.toLowerCase().includes(checkWord)) {
+                    msg.delete();
+                    var embed = new EmbedBuilder()
+                        .setColor("ff0000")
+                        .setDescription(`<@${msg.author.id}> (${msg.author.username}#${msg.author.discriminator}), links aren't allowed in this server without special permission. Please keep this community fun and safe for everyone!`)
+                    msg.channel.send({
+                        content: `<@${msg.author.id}> (${msg.author.username}#${msg.author.discriminator})`,
+                        embeds: [embed],
                     }).then((sentMsg) => {
                         setTimeout(() => {
                             sentMsg.delete();
