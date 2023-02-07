4f3247f2dde60274fe53c14289419703ee44acb6
TikoGrantYT <admin@tikogrant.com>
2023-02-06 17:04:52 -0800

    commit

--- commands/slay.js
+++ commands/slay.js
@@ -9,13 +9,13 @@ module.exports = {
 
     cooldowns: {
         type: CooldownTypes.perUserPerGuild,
-        duration: "2 h",
+        duration: "15 m",
     },
 
     callback: ({ interaction, client }) => {
         const embed = new EmbedBuilder()
             .setColor("ff73fa")
-            .setDescription(`**Slay!**\n<@${interaction.user.id}> (${interaction.user.username}#${interaction.user.discriminator}) has slayed the day away!\nYou can slay the day away by using </${interaction.commandName}:${interaction.commandId}>!\n\n*Psst!* You can only use this command every 2 hours. You are need to have the <@&1065776474285690961> role found in <#908404862356709446>`)
+            .setDescription(`**Slay!**\n<@${interaction.user.id}> (${interaction.user.username}#${interaction.user.discriminator}) has slayed the day away!\nYou can slay the day away by using </${interaction.commandName}:${interaction.commandId}>!\n\n*Psst!* You can only use this command every 2 hours. You need to have the <@&1065776474285690961> role found in <#908404862356709446>`)
 
         interaction.reply({
             embeds: [embed],
