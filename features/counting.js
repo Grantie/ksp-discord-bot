2d7b2b328732bf01f8c6158ef260057b830cbb32
TikoGrantYT <admin@tikogrant.com>
2023-03-11 12:27:00 -0800

    commit

--- features/counting.js
+++ features/counting.js
@@ -14,8 +14,17 @@ const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 module.exports = (client) => {
     client.on("messageCreate", (msg) => {
-        get(ref(db, "/counting")).then((counting) => {
+        get(ref(db, "/counting")).then(async (counting) => {
             if (!msg.author.bot && msg.channel.id === `${counting.val().channel}`) {
+                const webhooks = await msg.channel.fetchWebhooks();
+                const webhook = webhooks.find(wh => wh.channelId == msg.channel.id);
+
+                if (!webhook) {
+                    await msg.channel.createWebhook({
+                        name: "Counting Wehbook",
+                        avatar: "https://i.imgur.com/TAOr7Qx.png",
+                    });
+                }
                 // message sent in counting channel...
                 if (!isNaN(msg.content)) {
                     var saidNumber = parseInt(msg.content);
@@ -35,13 +44,29 @@ module.exports = (client) => {
                         set(ref(db, "/counting/previousCounter"), msg.author.id);
                         set(ref(db, "/counting/count"), counting.val().count + 1);
                         msg.react("<:ksp_check:1080663475321327737>");
+
+                        await webhook.send({
+                            content: `<:ksp_check:1080663475321327737> ${saidNumber.toString()}\n:bulb: *The next number is ${parseInt(counting.val().count) + 1}.*`,
+                            username: `Counted By: ${msg.author.username}#${msg.author.discriminator}`,
+                        });
+                        msg.delete();
                     } else {
                         msg.react("<:ksp_cross:1080663478265720922>");
-                        setTimeout(() => {
-                            msg.delete();
-                        }, 5000);
+                        await webhook.send({
+                            content: `<:ksp_cross:1080663478265720922> ${saidNumber.toString()}\n:bulb: *The next number is ${parseInt(counting.val().count) + 1}.*`,
+                            username: `Incorrectly Counted By: ${msg.author.username}#${msg.author.discriminator}`,
+                        });
+                        msg.delete();
                     }
                 } else {
+                    await webhook.send({
+                        content: `:notepad_spiral: ${msg.content}\n:bulb: *Deleting in less than 10 seconds.*`,
+                        username: `Note By: ${msg.author.username}#${msg.author.discriminator}`,
+                    }).then((webhookMsg) => {
+                        setTimeout(() => {
+                            webhookMsg.delete();
+                        }, 10000);
+                    });
                     msg.delete();
                 }
             }
